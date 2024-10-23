b9b9d459835d5fbd8473fef60e0d8b70be72cb6d
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-10-23 15:00:45 +0530

    add

--- src/util/baseURL.js
+++ src/util/baseURL.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with the base URL
 const api = axios.create({
-  baseURL: 'https://34.204.125.43:3000/api', 
+  baseURL: 'https://www.recipe.tequilasl.com/api', 
   headers: {
     'Content-Type': 'application/json',
   }
